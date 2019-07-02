dfc78b14e3a0762c6279eda2632ce94563682fd5
Andrzej Szary <aszary@gmail.com>
2019-07-02 18:53:35 +1000

    ArgParse added

---
+++ modules/data.jl
@@ -0,0 +1,14 @@
+module Data
+    using FITSIO
+    """
+    Loads data in FITS format
+    """
+    function load_fits(filename)
+        f = FITS(filename)
+        for hdu in f; println(typeof(hdu)); end
+        he = read_header(f[5])
+        println(he)
+        close(f)
+    end
+
+end # module
--- spats.jl
+++ spats.jl
@@ -1,1 +1,34 @@
+module SpaTs
+    using ArgParse
+
+    include("modules/data.jl")
+
+    function main()
+        parsed_args = parse_commandline()
+        for (arg,val) in parsed_args
+            println("  $arg  =>  $val")
+        end
+    end
+
+    function parse_commandline()
+        s = ArgParseSettings()
+        @add_arg_table s begin
+            "--indir", "-i"
+                help = "input directory"
+                default = "input"
+            "--outdir", "-o"
+                help = "output directory"
+                default = "output"
+            "--plot", "-p"
+                help = "plots to create"
+                default = []
+                nargs = '*'
+        end
+        return parse_args(s)
+    end
+
+end # module
+
+SpaTs.main()
+
 println("Bye")
