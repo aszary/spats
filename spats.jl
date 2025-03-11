193467c3ea2a1a9a2447a9b718df3578b8235290
Andrzej Szary <aszary@gmail.com>
2025-03-11 13:42:27 +0100

    PSR J1319 added +

--- spats.jl
+++ spats.jl
@@ -985,8 +985,8 @@ module SpaTs
         Data.convert_psrfit_ascii("/home/psr/data/new/J1319-6105/2019-12-15-03:19:04/2019-12-15-03:19:04_00000-00255.spCF", outdir*"1.txt")  
         data = Data.load_ascii(outdir*"1.txt")
         Plot.single(data, outdir; darkness=0.5, number=nothing, bin_st=400, bin_end=600, start=1, name_mod="J1319", show_=true)
-        Plot.average(data, outdir; number=nothing, bin_st=400, bin_end=600, start=1, name_mod="J1319", show_=true)
-        Plot.lrfs(data, outdir; darkness=0.1, start=1, name_mod="J1319", bin_st=400, bin_end=600, show_=true)
+        Plot.average(data, outdir; number=nothing, bin_st=400, bin_end=600, start=1, name_mod="J1319")
+        Plot.lrfs(data, outdir; darkness=0.1, start=1, name_mod="J1319", bin_st=400, bin_end=600)
     end
 
     function main()
