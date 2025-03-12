dbbe78bf71499d6b1d7ccdb302534f9384add796
Maciej <macszn@st.amu.edu.pl>
2025-03-12 11:35:08 +0100

    bin

--- spats.jl
+++ spats.jl
@@ -991,9 +991,9 @@ module SpaTs
     function J9040(outdir)
         Data.convert_psrfit_ascii("/home/psr/data/new/J1720+2150/2020-08-20-19:12:27/2020-08-20-19:12:27_00000-00255.spCF" , "/home/psr/output/J1720+2150.txt")
         data = Data.load_ascii("/home/psr/output/J1720+2150.txt")
-        Plot.single(data, outdir, darkness=0.5, bin_st=1 , bin_end=1024, number=nothing, name_mod="J1720+2150", show_=true)
-        Plot.lrfs(data, outdir, darkness=0.1, start=1,  bin_st=1, bin_end=1024, name_mod="J1720+2150", change_fftphase=false, show_=true)
-        Plot.average(data, outdir, bin_st=1, bin_end=1024, number=nothing, name_mod="J1720+2150", show_=true)
+        Plot.single(data, outdir, darkness=0.5, bin_st=1 , bin_end=250, number=nothing, name_mod="J1720+2150", show_=true)
+        Plot.lrfs(data, outdir, darkness=0.1, start=1,  bin_st=1, bin_end=250, name_mod="J1720+2150", change_fftphase=false, show_=true)
+        Plot.average(data, outdir, bin_st=1, bin_end=250, number=nothing, name_mod="J1720+2150", show_=true)
     end
     function main()
         # output directory for local run
