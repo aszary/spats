1d2e120f3d15e42ba85e96601642515bbd803787
Maciej <macszn@st.amu.edu.pl>
2025-03-12 11:25:04 +0100

    00767

--- spats.jl
+++ spats.jl
@@ -989,11 +989,11 @@ module SpaTs
     end
 
     function J9040(outdir)
-        Data.convert_psrfit_ascii("/home/psr/data/new/J0904-4246/2019-12-15-01:07:34/2019-12-15-01:07:34_00000-00255.spCf16" , "/home/psr/output/J0904-4246_16.txt")
-        data = Data.load_ascii("/home/psr/output/J0904-4246_16.txt")
-        Plot.single(data, outdir, darkness=0.5, bin_st=1 , bin_end=1024, number=nothing, name_mod="J0904_16", show_=true)
-        Plot.lrfs(data, outdir, darkness=0.1, start=1,  bin_st=1, bin_end=1024, name_mod="J0904_16", change_fftphase=false, show_=true)
-        Plot.average(data, outdir, bin_st=1, bin_end=1024, number=nothing, name_mod="J0904_16", show_=true)
+        Data.convert_psrfit_ascii("/home/psr/data/new/J0904-4246/2019-12-15-01:07:34/2019-12-15-01:07:34_00512-00767.spCF" , "/home/psr/output/J0904-4246_00767.txt")
+        data = Data.load_ascii("/home/psr/output/J0904-4246_00767.txt")
+        Plot.single(data, outdir, darkness=0.5, bin_st=1 , bin_end=1024, number=nothing, name_mod="J0904_00767", show_=true)
+        Plot.lrfs(data, outdir, darkness=0.1, start=1,  bin_st=1, bin_end=1024, name_mod="J0904_00767", change_fftphase=false, show_=true)
+        Plot.average(data, outdir, bin_st=1, bin_end=1024, number=nothing, name_mod="J0904_00767", show_=true)
     end
     function main()
         # output directory for local run
