c403cf192902f53a917d20770a3e09ee1c2ff62d
Andrzej Szary <aszary@gmail.com>
2025-03-27 10:55:04 +0100

    p3fold...

--- modules/data.jl
+++ modules/data.jl
@@ -148,7 +148,6 @@ Mark signal with two mouse clicks and press S""")
         debased_file = replace(outfile, ".spCF" => ".debase.gg")
         println("debased_file: $debased_file")
         run(pipeline(`pspec -w -2dfs -lrfs -nfft 256 -onpulse "$(bin_st) $(bin_end)" $debased_file`,  stderr="errs.txt"))
-        return
 
         run(pipeline(`pspecDetect -v $debased_file`, `tee pspecDetect_output.txt`))
         # Read captured output
