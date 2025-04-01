c4126965ee84749858a14664a9a3d3d4fc841a34
Andrzej Szary <aszary@gmail.com>
2025-04-01 13:56:28 +0200

    p3fold...

--- modules/data.jl
+++ modules/data.jl
@@ -152,7 +152,7 @@ module Data
 
         # Calculate 2dfs and lrfs
         #run(pipeline(`pspec -w -2dfs -lrfs  -onpulsed "/NULL"-2dfsd "/NULL"  -lrfsd "/NULL" -nfft 256 -onpulse "$(bin_st) $(bin_end)" $debased_file`,  stderr="errs.txt"))
-        run(pipeline(`pspec -w -2dfs -lrfs -onpulsed "/NULL"-nfft 256 -onpulse "$(bin_st) $(bin_end)" $debased_file`,  stderr="errs.txt"))
+        run(pipeline(`pspec -w -2dfs -lrfs -onpulsed "/NULL" -nfft 256 -onpulse "$(bin_st) $(bin_end)" $debased_file`,  stderr="errs.txt"))
 
         # Find P3
         run(pipeline(`pspecDetect -v -device "/xw" $debased_file`, `tee pspecDetect_output.txt`))
