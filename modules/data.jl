699a24c0bae27cf39c12afea5540b54d444b0e86
Andrzej Szary <aszary@gmail.com>
2025-04-10 12:35:10 +0200

    cleaning...

--- modules/data.jl
+++ modules/data.jl
@@ -241,7 +241,7 @@ module Data
         twodfs_lrfs(debased_file, params_file, p)
 
 
-        run(pipeline(`pfold  -p3fold "$(p["p3_value"]) $(p["ybins"])" -onpulse "$(p["bin_st"]) $(p["bin_end"])" -onpulsed "/NULL" -p3foldd "/NULL" -w -oformat ascii $debased_file`,  stderr="errs.txt"))
+        run(pipeline(`pfold  -p3fold "$(p["p3"]) $(p["ybins"])" -onpulse "$(p["bin_st"]) $(p["bin_end"])" -onpulsed "/NULL" -p3foldd "/NULL" -w -oformat ascii $debased_file`,  stderr="errs.txt"))
 
         return p
     end
