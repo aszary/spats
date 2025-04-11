0f7246108ba26c9a28c4a29a337638522689fdfa
Czk18 <czkk18@gmail.com>
2025-04-11 10:41:12 +0200

    zmiany

--- spats.jl
+++ spats.jl
@@ -1092,7 +1092,7 @@ module SpaTs
     
     function process_psrdata(indir, outdir)
         bin_st, bin_end, pulsar_name = Data.process_psrdata(indir, outdir)
-        folded = Data.load_ascii(joinpath(outdir, pulsar_name, "$pulsar_name.debase.p3fold"))
+        folded = Data.load_ascii(joinpath(outdir, pulsar_name, "pulsar.debase.p3fold"))
         Plot.p3fold(folded, joinpath(outdir, pulsar_name); start=3, bin_st=bin_st, bin_end=bin_end, name_mod="test", show_=true, repeat_num=4)
         
     end
