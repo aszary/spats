5e6b6cf7b437d0655a07e6d6925b6731329cd503
Andrzej Szary <aszary@gmail.com>
2025-04-15 13:58:39 +0200

    transfer from andrzej branch in progress...

--- modules/plot.jl
+++ modules/plot.jl
@@ -55,9 +55,11 @@ module Plot
         xlim(longitude[1], longitude[end])
         xlabel("longitude \$(^\\circ)\$")
         #tick_params(labeltop=false, labelbottom=true)
+        println("$outdir/average_$name_mod.pdf")
         savefig("$outdir/average_$name_mod.pdf")
         if show_ == true
             show()
+            println("Press Enter to close figure.")
             readline(stdin; keep=false)
         end
         close()
@@ -116,10 +118,147 @@ module Plot
         savefig("$outdir/$(name_mod)_single.pdf")
         if show_ == true
             show()
+            println("Press Enter to close figure.")
             readline(stdin; keep=false)
         end
         close()
         #clf()
+    end
+
+
+    function lrfs(data, outdir; start=1, number=nothing, cmap="viridis", bin_st=nothing, bin_end=nothing, darkness=0.5, name_mod="0", change_fftphase=true, show_=false)
+
+        num, bins = size(data)
+        if number == nothing
+            number = num - start  # missing one?
+        end
+        if bin_st == nothing bin_st = 1 end
+        if bin_end == nothing bin_end = bins end
+        da = data[start:start+number-1,bin_st:bin_end]
+        average = Tools.average_profile(da)
+        lrfs, intensity, freq, peak = Tools.lrfs(da)
+        println("\tpeak freq $(freq[peak]) ")
+        println("\tpeak P3 $(1/freq[peak])")
+        phase_ = rad2deg.(angle.(view(lrfs, peak, :)))  # fft phase variation  # view used! lrfs[peak, :] -> copies data
+        # skip freq = 0 and normalize intensity to 1
+        inten = intensity[2:end]
+        inten .-= minimum(inten)
+        inten ./= maximum(inten)
+        fre = freq[2:end]
+        pars, errs = Tools.fit_gaussian(fre, inten; μ=freq[peak-1])  # skip zero freq
+        fr = pars[2]
+        frer = pars[3]  # errs[2] # yeap
+
+        println("\tFrequency (gaussian fit): $fr, P3: $(1/fr)")
+        println("\tFrequency error (gaussian fit): $frer, P3 error: $(1/fr - 1/(fr +frer))")  # TODO err ok?
+
+        # Pulse longitude
+        db = (bin_end + 1) - bin_st  # yes +1
+        dl = 360. * db / bins
+        longitude = collect(range(-dl/2., dl/2., length=db))
+
+        # fixing phase continuity, estimating phase change
+        dl = longitude[2]-longitude[1]
+        dphase = zeros(length(phase_)-1)
+        println("Longitude resolution = ", dl, " (deg.)")
+        if change_fftphase == true
+            for i in 1:length(phase_)-1
+                global dp
+                changed = true
+                while changed
+                    changed = false
+                    dp = abs(phase_[i+1] - phase_[i])
+                    dpp = abs(phase_[i+1]+360. - phase_[i])
+                    dpm = abs(phase_[i+1]-360. - phase_[i])
+                    if dpp < dp
+                        phase_[i+1] += 360
+                        changed = true
+                    end
+                    if dpm < dp
+                        phase_[i+1] -= 360
+                        changed = true
+                    end
+                end
+                dphase[i] = dp
+                #println("$i $(longitude[i])  $dp")
+            end
+        end
+        # longitude vs. FFT phase analysis
+
+        #=
+        lin, incs = Tools.find_inclination(longitude, phase_)
+        for (i,inc) in enumerate(incs)
+            println("($i) Δ FFT phase / Δ longitude ", round(inc[2], digits=2))
+            println("($i) sigma Δ FFT phase / Δ longitude ", round(inc[3], digits=2))
+        end
+        =#
+
+        lin, inc = Tools.find_inclination2(longitude, phase_)
+        println(" Δ FFT phase / Δ longitude ", round(inc[1][2], digits=2))
+        println("sigma Δ FFT phase / Δ longitude ", round(inc[1][3], digits=2))
+
+        rc("font", size=7.)
+        rc("axes", linewidth=0.5)
+        rc("lines", linewidth=0.5)
+
+        figure(figsize=(3.14961, 4.33071))  # 8cm x 11cm
+        subplots_adjust(left=0.17, bottom=0.08, right=0.90, top=0.92, wspace=0., hspace=0.)
+
+        ax = subplot2grid((5, 3), (0, 1), colspan=2)
+        minorticks_on()
+        #plot(longitude, phase_, c="grey")
+        scatter(longitude, phase_, marker=".", c="grey", s=3.)
+        ax.xaxis.set_label_position("top")
+        ax.xaxis.set_ticks_position("top")
+        for l in lin
+            plot(l[1], l[2])
+        end
+        #ylim(-500, 0)
+        #xlim(-1, 2)
+        xlabel("longitude \$(^\\circ)\$")
+        ylabel("FFT phase \$(^\\circ)\$")
+        if change_fftphase == true
+            ax2 = ax.twinx()
+            minorticks_on()
+            plot(longitude[7:end-9], dphase[6:end-9])
+            ax2.xaxis.set_label_position("top")
+        end
+        tick_params(labeltop=true, labelbottom=false, which="both", bottom=false, top=true)
+
+        subplot2grid((5, 3), (1, 0), rowspan=3)
+        minorticks_on()
+        plot(inten, fre, c="grey")
+        plot(Tools.gauss(fre, pars), fre, c="red", ls=":", lw=0.3)
+        ylim(freq[1], freq[end])
+        xticks([0.5, 1.0])
+        xlim(1.1, -0.1)
+        xlabel("intensity")
+        ylabel("frequency \$(1/P)\$")
+
+        subplot2grid((5, 3), (1, 1), rowspan=3, colspan=2)
+        imshow(abs.(lrfs), origin="lower", cmap=cmap, interpolation="none", aspect="auto", vmax=darkness*maximum(abs.(lrfs)))
+        #println(size(lrfs))
+        #return
+
+        tick_params(labelleft=false, labelbottom=false)
+
+        subplot2grid((5, 3), (4, 1), colspan=2)
+        minorticks_on()
+        plot(longitude, average, c="grey")
+        axvline(x=0., ls=":", c="black")
+        yticks([0.0, 0.5])
+        xlim(longitude[1], longitude[end])
+        xlabel("longitude \$(^\\circ)\$")
+        #tick_params(labeltop=false, labelbottom=true)
+        println("$outdir/$(name_mod)_lrfs.pdf")
+        savefig("$outdir/$(name_mod)_lrfs.pdf")
+        savefig("$outdir/$(name_mod)_lrfs.svg")
+        if show_ == true
+            show()
+            println("Press Enter to close figure.")
+            readline(stdin; keep=false)
+        end
+        close()
     end
 
 
