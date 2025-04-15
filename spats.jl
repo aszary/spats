de2474b2fda59f7ed17f811b37c220b4f0d9dd39
Andrzej Szary <aszary@gmail.com>
2025-04-15 12:48:12 +0200

    transfer from andrzej branch in progress...

--- spats.jl
+++ spats.jl
@@ -17,12 +17,31 @@ module SpaTs
 
     end
 
+    function test2(outdir)    
+        Data.convert_psrfit_ascii("/home/psr/data/new/J1319-6105/2019-12-15-03:19:04/2019-12-15-03:19:04_00000-00255.spCF", outdir*"1.txt")  
+        Data.convert_psrfit_ascii("/home/psr/data/new/J1319-6105/2019-12-15-03:19:04/2019-12-15-03:19:04_00256-00511.spCF", outdir*"2.txt")  
+        Data.convert_psrfit_ascii("/home/psr/data/new/J1319-6105/2019-12-15-03:19:04/2019-12-15-03:19:04_00512-00767.spCF", outdir*"3.txt")  
+        Data.convert_psrfit_ascii("/home/psr/data/new/J1319-6105/2019-12-15-03:19:04/2019-12-15-03:19:04_00768-01029.spCF", outdir*"4.txt")  
+        data1 = Data.load_ascii(outdir*"1.txt")
+        data2 = Data.load_ascii(outdir*"2.txt")
+        data3 = Data.load_ascii(outdir*"3.txt")
+        data4 = Data.load_ascii(outdir*"4.txt")
+        data = vcat(data1, data2, data3, data4)
+        Plot.single(data, outdir; darkness=0.5, number=nothing, bin_st=400, bin_end=600, start=1, name_mod="J1319", show_=true)
+        Plot.average(data, outdir; number=nothing, bin_st=400, bin_end=600, start=1, name_mod="J1319", show_=true)
+        Plot.lrfs(data, outdir; darkness=0.1, start=1, name_mod="J1319", bin_st=400, bin_end=600, show_=true)
+        folded = Tools.fold_single(data, 20, 40)
+        Plot.single(folded, outdir; darkness=0.5, number=nothing, bin_st=400, bin_end=600, start=1, name_mod="J1319", show_=true)
+    end
+
 
     function main()
         # output directory for VPM
         vpmout = "/home/psr/output/"
 
-        test(vpmout)
+        #test(vpmout)
+        test2(vpmout)
+        #process_psrdata("/home/psr/data/new/J1919+0134/2020-02-02-11:45:29/", vpmout)
 
     end
 
