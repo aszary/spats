81845b98cc936a8f5dea77c139d50ed6ff0eb9f8
Czk18 <czkk18@gmail.com>
2025-04-15 13:27:31 +0200

    zmiany

--- spats.jl
+++ spats.jl
@@ -1407,7 +1407,7 @@ end
         #process_psrdata("/home/psr/data/new/J1057-5226/2019-06-21-15:37:29", vpmout)
         #print_lrfs_header_from_folder("~/output/J1919+0134")
 
-        plot_2dfs("/home/psr/output", "J1919+0134", show_plot=true)
+        plot_2dfs("/home/psr/output", "J1057-5226", show_plot=true)
         #inspect_fits("/home/psr/output/J1919+0134/pulsar.debase.1.2dfs")
         #print_first_10_lines("/home/psr/output/J1057-5226/pulsar.debase.1.2dfs")
 
