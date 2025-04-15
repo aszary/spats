b2d7eb2ace9951edf9a99e39b84f3c50c1bdb701
Maciej <macszn@st.amu.edu.pl>
2025-04-15 14:23:15 +0200

    params?

--- spats.jl
+++ spats.jl
@@ -1171,9 +1171,14 @@ end
         println("Parameters updated and saved to $params_file")
     
         # Step 10: Plot
+        Plot.single(data, outdir;darkness=0.5,bin_st=p["bin_st"],bin_end=p["bin_end"],start=p["pulse_start"],number=p["number"],name_mod="J0820Mac",show_=true)
+        Plot.lrfs(data, outdir;darkness=0.1,start=p["pulse_start"],bin_st=p["bin_st"],bin_end=p["bin_end"],name_mod="J0820Mac",change_fftphase=false,show_=true)
+        Plot.average(data, outdir;bin_st=p["bin_st"],bin_end=p["bin_end"],number=nothing,name_mod="J0820Mac",show_=false)
+#=
         Plot.single(combined_data, output_subdir, darkness=0.5, bin_st=bin_st, bin_end=bin_end, number=nothing, name_mod=name_mod, show_=false)
         Plot.lrfs(combined_data, output_subdir, darkness=0.1, start=1, bin_st=bin_st, bin_end=bin_end, name_mod=name_mod, change_fftphase=false, show_=false)
         Plot.average(combined_data, output_subdir,bin_st=bin_st, bin_end=bin_end, number=nothing, name_mod=name_mod, show_=false)
+        =#
     end
     
 
