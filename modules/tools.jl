57afe0025656fc8cf834564b0f9b70d0ffa29a25
Andrzej Szary <aszary@gmail.com>
2025-04-15 15:01:10 +0200

    transfer from andrzej branch in progress...

--- modules/tools.jl
+++ modules/tools.jl
@@ -2061,5 +2061,75 @@ module Tools
         return h / 3 * s
     end
 
+    
+    """
+    Creates folded profile
+    :param data: single pulses
+    :param p3: P_3 periodicity
+    :return: folded profile
+    """
+    function p3fold(data, p3, ybins=10, start_ind=nothing)
+        if isnothing(start_ind)
+            start_ind = Int(floor(p3 / 2))
+        end
+    
+        data = data[start_ind+1:end, :]
+    
+        folded_ = zeros(Float64, ybins, size(data, 2))
+    
+        dp3 = p3 / ybins
+    
+        for i in 1:size(data, 1)
+            new_ind = i / dp3
+            j = Int(floor(mod(new_ind, ybins)))
+            folded_[j+1, :] += data[i, :]
+        end
+    
+        return folded_
+    end
+
+    """
+    Save parameters used in the analysis to a JSON file
+    """
+    function save_params(filename, params)
+        open(filename, "w") do file
+            JSON.print(file, params)
+        end
+    end
+
+
+    """
+    Read parameters used in the analysis from a JSON file
+    """
+    function read_params(filename)
+        f = open(filename, "r")
+        params = JSON.parse(f)
+        close(f)
+        return params
+    end
+
+
+    """
+    Write default parameters to a JSON file
+    """
+    function default_params(filename)
+        p = Dict(
+            "nfft" => 256,
+            "_nfft" => "Set size of fft's [default=256]",
+            "bin_st" => nothing,
+            "bin_end" => nothing,
+            "pulse_start" => 1,
+            "pulse_end" => 150,
+            "nsubint" => nothing,
+            "p3" => -1.0,
+            "p3_error" => -1.0,
+            "p3_ybins" => nothing
+        )
+        f = open(filename, "w")
+        JSON.print(f, p)
+        close(f)
+        return p
+    end
+
 
 end  # module Tools
