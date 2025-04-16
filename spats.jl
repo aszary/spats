73373f3c318d21e78fd197406402eeb15707ab45
Andrzej Szary <aszary@gmail.com>
2025-04-16 10:10:24 +0200

    transfer from andrzej branch in progress...

--- spats.jl
+++ spats.jl
@@ -9,12 +9,10 @@ module SpaTs
 
 
     function test(outdir)
-
         d = Data.load_ascii("input/1.txt")
         Plot.single(d, outdir; darkness=0.3, number=256, bin_st=400, bin_end=600, start=1, name_mod="1", show_=true)
-        Plot.average(d, outdir; number=256, bin_st=400, bin_end=600, start=1, name_mod="1")
-        Plot.lrfs(d, outdir; darkness=0.1, start=1, name_mod="1", bin_st=500, bin_end=530)
-
+        Plot.average(d, outdir; number=256, bin_st=400, bin_end=600, start=1, name_mod="1", show_=true)
+        Plot.lrfs(d, outdir; darkness=0.1, start=1, name_mod="1", bin_st=500, bin_end=530, show_=true)
     end
 
     function test2(outdir)    
