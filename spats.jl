6ce9858579ee5c95a40d8c0aad9e8881a34c7f97
Maciej <macszn@st.amu.edu.pl>
2025-04-29 14:01:01 +0200

    noshow

--- spats.jl
+++ spats.jl
@@ -1171,9 +1171,9 @@ end
         println("Parameters updated and saved to $params_file")
     
         # Step 10: Plot
-        Plot.single(combined_data, output_subdir; darkness=0.5, bin_st=p["bin_st"], bin_end=p["bin_end"], start=p["pulse_start"], number=(p["pulse_end"]-p["pulse_start"]), name_mod=name_mod, show_=true)
+        Plot.single(combined_data, output_subdir; darkness=0.5, bin_st=p["bin_st"], bin_end=p["bin_end"], start=p["pulse_start"], number=(p["pulse_end"]-p["pulse_start"]), name_mod=name_mod, show_=false)
 
-        Plot.lrfs(combined_data, output_subdir; darkness=0.1, start=p["pulse_start"], bin_st=p["bin_st"], bin_end=p["bin_end"], name_mod=name_mod, change_fftphase=false, show_=true)
+        Plot.lrfs(combined_data, output_subdir; darkness=0.1, start=p["pulse_start"], bin_st=p["bin_st"], bin_end=p["bin_end"], name_mod=name_mod, change_fftphase=false, show_=false)
 
         Plot.average(combined_data, output_subdir; bin_st=p["bin_st"], bin_end=p["bin_end"], number=(p["pulse_end"]-p["pulse_start"]), name_mod=name_mod, show_=false)
         
