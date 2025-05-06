1c807a01fc1ffdfc10459c51470aec365b3222d1
Czk18 <czkk18@gmail.com>
2025-05-06 14:00:40 +0200

    threshold=0.3

--- spats.jl
+++ spats.jl
@@ -94,10 +94,10 @@ module SpaTs
         combined_data = vcat(data1, data2, data3, data4)
     
         Plot.single(combined_data, vpmout; darkness=0.5, number=nothing, bin_st=400, bin_end=600, start=1, name_mod="J1319", show_=true)
-        Plot.average(combined_data, vpmout; number=nothing, bin_st=400, bin_end=600, start=1, name_mod="J1319", show_=true)
-        Plot.lrfs(combined_data, vpmout; darkness=0.1, start=1, name_mod="J1319", bin_st=400, bin_end=600, show_=true)
-        folded = Tools.p3fold(combined_data, 20, 40)
-        Plot.single(folded, vpmout; darkness=0.5, number=nothing, bin_st=400, bin_end=600, start=1, name_mod="J1319_p3fold", show_=true)
+        #Plot.average(combined_data, vpmout; number=nothing, bin_st=400, bin_end=600, start=1, name_mod="J1319", show_=true)
+        #Plot.lrfs(combined_data, vpmout; darkness=0.1, start=1, name_mod="J1319", bin_st=400, bin_end=600, show_=true)
+        #folded = Tools.p3fold(combined_data, 20, 40)
+        #Plot.single(folded, vpmout; darkness=0.5, number=nothing, bin_st=400, bin_end=600, start=1, name_mod="J1319_p3fold", show_=true)
     end
     
     
