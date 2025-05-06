503edbe791c2f34dd932e21c719a77fbb34b3204
Czk18 <czkk18@gmail.com>
2025-05-06 14:36:32 +0200

    threshold=0.0

--- spats.jl
+++ spats.jl
@@ -18,15 +18,15 @@ module SpaTs
 
     
     function test2(outdir)    
-        Data.convert_psrfit_ascii("/home/psr/data/new/J1232-4742/2020-04-11-22:21:18/2020-04-11-22:21:18_00000-00255.spCF", outdir*"1.txt")  
-        Data.convert_psrfit_ascii("/home/psr/data/new/J1232-4742/2020-04-11-22:21:18/2020-04-11-22:21:18_00256-00511.spCF", outdir*"2.txt")  
-        Data.convert_psrfit_ascii("/home/psr/data/new/J1232-4742/2020-04-11-22:21:18/2020-04-11-22:21:18_00512-00767.spCF", outdir*"3.txt")  
-        Data.convert_psrfit_ascii("/home/psr/data/new/J1232-4742/2020-04-11-22:21:18/2020-04-11-22:21:18_00768-00961.spCF", outdir*"4.txt")  
+        Data.convert_psrfit_ascii("/home/psr/data/new/J1414-6802/2020-07-24-16:17:58/2020-07-24-16:17:58_00000-00255.spCF", outdir*"1.txt")  
+        Data.convert_psrfit_ascii("/home/psr/data/new/J1414-6802/2020-07-24-16:17:58/2020-07-24-16:17:58_00256-00537.spCF", outdir*"2.txt")  
+        #Data.convert_psrfit_ascii("/home/psr/data/new/J1414-6802/2020-07-24-16:17:58/2020-07-24-16:17:58_00512-00767.spCF", outdir*"3.txt")  
+        #Data.convert_psrfit_ascii("/home/psr/data/new/J1414-6802/2020-07-24-16:17:58/2020-07-24-16:17:58_00768-00961.spCF", outdir*"4.txt")  
         data1 = Data.load_ascii(outdir*"1.txt")
         data2 = Data.load_ascii(outdir*"2.txt")
-        data3 = Data.load_ascii(outdir*"3.txt")
-        data4 = Data.load_ascii(outdir*"4.txt")
-        data = vcat(data1, data2, data3, data4)
+        #data3 = Data.load_ascii(outdir*"3.txt")
+        #data4 = Data.load_ascii(outdir*"4.txt")
+        data = vcat(data1, data2)
         Plot.single(data, outdir; darkness=0.5, number=nothing, bin_st=300, bin_end=700, start=1, name_mod="J1232-4742", show_=true)
         #Plot.average(data, outdir; number=nothing, bin_st=300, bin_end=700, start=1, name_mod="J1232-4742", show_=true)
         Plot.lrfs(data, outdir; darkness=0.1, start=1, name_mod="J1232-4742", bin_st=300, bin_end=700, show_=true)
@@ -93,9 +93,9 @@ module SpaTs
         
         combined_data = vcat(data1, data2, data3, data4)
     
-        Plot.single(combined_data, vpmout; darkness=0.5, number=nothing, bin_st=400, bin_end=600, start=1, name_mod="J1232-4742", show_=true)
+        Plot.single(combined_data, vpmout; darkness=0.5, number=nothing, bin_st=300, bin_end=700, start=1, name_mod="J1414-6802", show_=true)
         #Plot.average(combined_data, vpmout; number=nothing, bin_st=400, bin_end=600, start=1, name_mod="J1319", show_=true)
-        Plot.lrfs(combined_data, vpmout; darkness=0.1, start=1, name_mod="J1232-4742", bin_st=400, bin_end=600, show_=true)
+        Plot.lrfs(combined_data, vpmout; darkness=0.1, start=1, name_mod="J1414-6802", bin_st=300, bin_end=700, show_=true)
         #folded = Tools.p3fold(combined_data, 20, 40)
         #Plot.single(folded, vpmout; darkness=0.5, number=nothing, bin_st=400, bin_end=600, start=1, name_mod="J1319_p3fold", show_=true)
     end
