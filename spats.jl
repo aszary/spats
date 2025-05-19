e68cbe61f41c9d941c6d568751fd92679fcc5db4
Czk18 <czkk18@gmail.com>
2025-05-19 22:04:40 +0200

    2dfs-wykres-pspec

--- spats.jl
+++ spats.jl
@@ -582,7 +582,7 @@ function Plot_ostateczny_simple(outdir::String, pulsar_name::String, period::Flo
                 minimum(P_over_P3[2:end]), maximum(P_over_P3[2:end])],
         cmap="gray",
         vmin=0.0,
-        vmax= 5000
+        vmax= 2000.0
     )
     #im.set_clim(0.0, 0.07)
 
