8d044727f21cda20a5a28202c6c512a83e5d1517
Czk18 <czkk18@gmail.com>
2025-05-20 14:07:48 +0200

    2dfs-wykres-pspec

--- modules/plot.jl
+++ modules/plot.jl
@@ -151,7 +151,7 @@ module Plot
         rc("axes", linewidth=0.5)
         rc("lines", linewidth=0.5)
 
-        figure(figsize=(3.14961, 4.33071), frameon=true)  # 8cm x 11cm
+        figure(figsize=(6.29922, 8.66142), frameon=true)  # 16cm x 22cm
         subplots_adjust(left=0.16, bottom=0.09, right=0.99, top=0.99, wspace=0.0, hspace=0.0)
 
         # LEWY wykres – fluktuacje
