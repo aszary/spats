535d76e492769afb6b05f225bece825cec5d6541
Czk18 <czkk18@gmail.com>
2025-05-20 15:02:25 +0200

    2dfs-wykres-pspec

--- modules/plot.jl
+++ modules/plot.jl
@@ -168,7 +168,7 @@ module Plot
         # ŚRODKOWY wykres – mapa intensywności
         subplot2grid((5, 3), (1, 1), rowspan=4, colspan=2)
         imshow(da, origin="lower", cmap=cmap, interpolation="none", aspect="auto",  vmax=darkness*maximum(da))
-        xlabel("Pulse longitude (deg)")
+        xlabel("Fluctuation frequency")
         tick_params(labelleft=false, labelbottom=true)
 
         # GÓRNY wykres – średni profil (dawny dolny)
