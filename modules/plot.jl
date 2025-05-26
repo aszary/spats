fdf3e63cf7650f4be5984d7fc4e21f363699b682
Czk18 <czkk18@gmail.com>
2025-05-26 18:23:23 +0200

    2dfs-wykres-pspec

--- modules/plot.jl
+++ modules/plot.jl
@@ -174,7 +174,7 @@ module Plot
         subplots_adjust(left=0.16, bottom=0.09, right=0.90, top=0.99, wspace=0.0, hspace=0.0)
 
         # Panel górny: average_x (profil poziomy)
-        subplot2grid((5, 3), (0, 1), colspan=1)
+        subplot2grid((5, 3), (0, 1), colspan=2)
         minorticks_on()
         plot(xrange, average_x, color="grey")
         yticks([])
