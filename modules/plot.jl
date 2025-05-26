8dd6511be3f6e6d43af23724ee26453281aba4a3
Czk18 <czkk18@gmail.com>
2025-05-26 21:36:49 +0200

    2dfs-wykres-pspec

--- modules/plot.jl
+++ modules/plot.jl
@@ -239,7 +239,7 @@ module Plot
                     cmap="gray_r",
                     bin_st=nothing,
                     bin_end=nothing,
-                    darkness=0.02,
+                    darkness=0.1,
                     name_mod="PSR_NAME",
                     show_=false)
 
