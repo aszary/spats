95109194ec3b5805d2d0310bdcb94cdf1ff822fd
Czk18 <czkk18@gmail.com>
2025-06-03 13:59:16 +0200

    process_psrdata2

--- spats.jl
+++ spats.jl
@@ -162,13 +162,13 @@ module SpaTs
         # output directory for VPM
         vpmout = "/home/psr/output/"
 
-        #d = Data.load_ascii("/home/psr/output/pulsar.debase.1.2dfs")
-        #println(size(d))
-        #Plot.twodfs_plot2(d, vpmout; show_=true)
-
-        d = Data.load_ascii_all("/home/psr/output/pulsar.debase.lrfs")
+        d = Data.load_ascii("/home/psr/output/pulsar.debase.1.2dfs")
         println(size(d))
-        Plot.lrfs_plot(d[:,:,3], vpmout; show_=true)
+        Plot.twodfs_plot2(d, vpmout; show_=true)
+
+        #d = Data.load_ascii_all("/home/psr/output/pulsar.debase.lrfs")
+        #println(size(d))
+        #Plot.lrfs_plot(d[:,:,3], vpmout; show_=true)
 
         #test(vpmout)
         #test2(vpmout)
