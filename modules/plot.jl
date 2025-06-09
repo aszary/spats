9e89d727d3edbd6441757b2aa494f617e0b2f89d
Andrzej Szary <aszary@gmail.com>
2025-06-09 10:58:11 +0200

    adding Czarek's code

--- modules/plot.jl
+++ modules/plot.jl
@@ -370,7 +370,7 @@ module Plot
         rc("lines", linewidth=0.5)
 
         figure(figsize=(3.14961, 4.33071))  # 8cm x 11cm
-        subplots_adjust(left=0.17, bottom=0.08, right=0.90, top=0.92, wspace=0., hspace=0.)
+        subplots_adjust(left=0.17, bottom=0.08, right=0.99, top=0.99, wspace=0., hspace=0.)
 
         # left panel
         ax_left = subplot2grid((4,3), (0,0), rowspan=3)
