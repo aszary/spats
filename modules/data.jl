f68110862dfdccf6b8eaca7ba835e49eb2bd36b7
Andrzej Szary <aszary@gmail.com>
2025-06-10 12:30:32 +0200

    adding Czarek's code

--- modules/data.jl
+++ modules/data.jl
@@ -316,7 +316,6 @@ module Data
         d3 = load_ascii(joinpath(data_dir, "pulsar.debase.lrfs"))
         Plot.lrfs(d3, data_dir, p; name_mod="pulsar", show_=false)
 
-
         # P3-folded
         d5 = Data.load_ascii(joinpath(data_dir, "pulsar.debase.p3fold"))
         Plot.p3fold(d5, data_dir; start=3, bin_st=p["bin_st"]-20, bin_end=p["bin_end"]+20, name_mod="pulsar", show_=false, repeat_num=4)
