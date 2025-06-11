181813938669eddbd68529a395826fd43642b283
Andrzej Szary <aszary@gmail.com>
2025-06-11 10:26:21 +0200

    Dipanjan's polarisation cleaning

--- modules/plot.jl
+++ modules/plot.jl
@@ -90,8 +90,9 @@ module Plot
         rc("axes", linewidth=0.5)
         rc("lines", linewidth=0.5)
 
-        figure(figsize=(3.14961, 4.33071), frameon=true)  # 8cm x 11cm
-        subplots_adjust(left=0.16, bottom=0.09, right=0.99, top=0.99, wspace=0., hspace=0.)
+        figure()
+        #figure(figsize=(3.14961, 4.33071), frameon=true)  # 8cm x 11cm
+        #subplots_adjust(left=0.16, bottom=0.09, right=0.99, top=0.99, wspace=0., hspace=0.)
 
         subplot2grid((5, 3), (0, 0), rowspan=4)
         minorticks_on()
