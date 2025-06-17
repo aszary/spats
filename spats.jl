ea123580d2e44a2b3f1ccae266894eea47b750b2
Maciej <macszn@st.amu.edu.pl>
2025-06-17 13:18:44 +0200

    test

--- spats.jl
+++ spats.jl
@@ -8,22 +8,10 @@ module SpaTs
     include("modules/tools.jl")
 
 
-    function test(outdir)
-        d = Data.load_ascii("input/1.txt")
-        Plot.single(d, outdir; darkness=0.3, number=256, bin_st=400, bin_end=600, start=1, name_mod="1", show_=true)
-        Plot.average(d, outdir; number=256, bin_st=400, bin_end=600, start=1, name_mod="1", show_=true)
-        Plot.lrfs_obsolete(d, outdir; darkness=0.1, start=1, name_mod="1", bin_st=500, bin_end=530, show_=true)
-    end
-
-
-
-  
-
-
     function main()
-        vpmout = "/home/psr/output/"
-        Data.process_all_data(vpmout)
-        
+        #vpmout = "/home/psr/output/"
+        #Data.process_all_data(vpmout)
+        Data.combine_4page(vpmout)
 
     end
 
