9b2d481ff7176acb241ae5144e6c024681499414
Czarek Kruszewski <czkk18@gmail.com>
2025-08-05 13:09:52 +0200

    zmiany

--- spats.jl
+++ spats.jl
@@ -50,7 +50,7 @@ module SpaTs
         #Plot.twodfs(d1, outdir, p; darkness=0.3, name_mod="pulsar", show_=true)
         # ADD Data.twodfs_lrfs...
         data.twodfs_lrfs(debased_file, outdir, p; darkness=0.3, name_mod="pulsar", show_=true)
-        Plot.lrfs(debased_file, outdir, p; darkness=0.3, name_mod="pulsar", show_=true)0
+        Plot.lrfs(debased_file, outdir, p; darkness=0.3, name_mod="pulsar", show_=true)
 
         #folded = Data.load_ascii(joinpath(outdir, "pulsar.debase.p3fold"))
         #Plot.p3fold(folded, outdir; start=3, bin_st=p["bin_st"]-20, bin_end=p["bin_end"]+20, name_mod="pulsar", show_=true, repeat_num=4)        
