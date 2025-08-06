52034806bbafba2f59e9b617d6f1de1fef9f1f7f
Andrzej Szary <aszary@gmail.com>
2025-08-06 10:36:54 +0200

    zmiany

--- spats.jl
+++ spats.jl
@@ -45,7 +45,7 @@ module SpaTs
         p = Tools.read_params(joinpath(outdir, "params.json"))
         d4 = Data.load_ascii_all(joinpath(outdir, "pulsar.debase.txt"))
         d1 = Data.clean(d4; threshold=0.0031)
-        Plot.single(d1, outdir; darkness=0.5, number=200, bin_st=p["bin_st"], bin_end=p["bin_end"], start=210, name_mod="pulsar", show_=true)
+        #Plot.single(d1, outdir; darkness=0.5, number=200, bin_st=p["bin_st"], bin_end=p["bin_end"], start=210, name_mod="pulsar", show_=true)
         #Plot.lrfs_obsolete(d1, outdir; darkness=0.3, start=210, name_mod="pulsar", bin_st=p["bin_st"], bin_end=p["bin_end"], show_=true)
         # ADD Data.twodfs_lrfs...
         Data.twodfs_lrfs(debased_file, outdir, p)
