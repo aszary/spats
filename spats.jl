3e9d81c2a615aac094e6fbd08a3ab5ce76a20822
Andrzej Szary <aszary@gmail.com>
2025-09-02 13:03:04 +0200

    zmiany

--- spats.jl
+++ spats.jl
@@ -45,7 +45,7 @@ module SpaTs
         p = Tools.read_params(joinpath(outdir, "params.json"))
         d4 = Data.load_ascii_all(joinpath(outdir, "pulsar.debase.txt"))
         d1 = Data.clean(d4; threshold=0.0031)
-        Plot.single(d1, outdir; darkness=0.5, number=200, bin_st=p["bin_st"], bin_end=p["bin_end"], start=210, name_mod="pulsar", show_=true)
+        Plot.single(d1, outdir; darkness=0.7, number=100, bin_st=p["bin_st"], bin_end=p["bin_end"], start=210, name_mod="pulsar", show_=true)
         Plot.lrfs_obsolete(d1, outdir; darkness=0.3, start=210, name_mod="pulsar", bin_st=p["bin_st"], bin_end=p["bin_end"], show_=true)
         Data.twodfs_lrfs(debased_file, outdir, p)
         lrfs_file = replace(debased_file, "gg"=>"lrfs")
@@ -57,6 +57,20 @@ module SpaTs
         folded = Data.load_ascii(joinpath(outdir, "pulsar.debase.p3fold"))
         Plot.p3fold(folded, outdir; start=3, bin_st=p["bin_st"]-20, bin_end=p["bin_end"]+20, name_mod="pulsar", show_=true, repeat_num=4)        
 
+    end
+
+    function J1539_6322_Sard(indir, outdir)
+        p = Data.process_psrdata(indir, outdir)
+        Data.convert_psrfit_ascii(joinpath(outdir, "pulsar.debase.gg"), joinpath(outdir, "pulsar.debase.txt"))
+
+        outfile="pulsar.spCF"
+        outfile = joinpath(outdir, outfile)
+        debased_file = replace(outfile, ".spCF" => ".debase.gg")
+
+        p = Tools.read_params(joinpath(outdir, "params.json"))
+        d4 = Data.load_ascii_all(joinpath(outdir, "pulsar.debase.txt"))
+        d1 = Data.clean(d4; threshold=0.00031)
+        Plot.single(d1, outdir; darkness=0.7, number=100, bin_st=p["bin_st"], bin_end=p["bin_end"], start=210, name_mod="pulsar", show_=true)
     end
 
     function process_psrdata(indir, outdir)
@@ -76,7 +90,7 @@ module SpaTs
         #test3("/home/psr/data/new/J1919+0134/2020-02-02-11:45:29/", vpmout)
         #test3("/home/psr/data/new/J1744-1610/2020-06-12-20:00:13/", vpmout)
         #process_psrdata("/home/psr/data/new/J1919+0134/2020-02-02-11:45:29/", vpmout)
-        test3("/home/psr/data/new/J1539-6322/2020-04-11-23:52:16/", vpmout)
+        J1539_6322_Sard("/home/psr/data/new/J1539-6322/2020-04-11-23:52:16/", vpmout)
 
         #Data.process_all_data(vpmout)
         #Data.combine_pngs_to_pdf(vpmout)
