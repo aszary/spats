ed8a045dc47aa60742576524336e9c57eaf236cd
Andrzej Szary <aszary@gmail.com>
2025-09-02 13:05:10 +0200

    zmiany

--- spats.jl
+++ spats.jl
@@ -69,8 +69,8 @@ module SpaTs
 
         p = Tools.read_params(joinpath(outdir, "params.json"))
         d4 = Data.load_ascii_all(joinpath(outdir, "pulsar.debase.txt"))
-        d1 = Data.clean(d4; threshold=0.31)
-        Plot.single(d1, outdir; darkness=0.7, number=100, bin_st=p["bin_st"], bin_end=p["bin_end"], start=210, name_mod="pulsar", show_=true)
+        d1 = Data.clean(d4; threshold=0.71)
+        Plot.single(d1, outdir; darkness=0.3, number=150, bin_st=p["bin_st"], bin_end=p["bin_end"], start=210, name_mod="pulsar", show_=true)
     end
 
     function process_psrdata(indir, outdir)
