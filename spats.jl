f4d022a01d1e1e8d1ef16d1a234eccfde5fd6e9d
Andrzej Szary <aszary@gmail.com>
2025-10-02 12:13:12 +0200

    zmiany

--- spats.jl
+++ spats.jl
@@ -94,9 +94,11 @@ module SpaTs
         #process_psrdata("/home/psr/data/new/J1919+0134/2020-02-02-11:45:29/", vpmout)
         #J1539_6322_Sard("/home/psr/data/new/J1539-6322/2020-04-11-23:52:16/", vpmout)
 
+        process_psrdata(vpmout*"J1921+2153", vpmout*"J1921+2153")
+
         #Data.process_all_data(vpmout)
         #Data.combine_pngs_to_pdf(vpmout)
-        Data.combine_pngs(vpmout)
+        #Data.combine_pngs(vpmout)
         
         #Data.remove_folders(vpmout)
         #Data.remove_notinteresting("input/pulsars_interesting.txt", vpmout)
