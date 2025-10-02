ccf0ff1199b462d5171e715d14ce01b82abf1c31
Andrzej Szary <aszary@gmail.com>
2025-10-02 12:39:33 +0200

    zmiany

--- modules/data.jl
+++ modules/data.jl
@@ -292,8 +292,10 @@ module Data
         # debase the data
         debase(outfile, params_file, p)
 
+        println("1---")
         # Calculate 2dfs and lrfs
         twodfs_lrfs(debased_file, params_file, p; detect=false)
+        println("2---")
 
         # calculate p3-folded profile
         run(pipeline(`pfold  -p3fold "$(p["p3"]) $(p["p3_ybins"])" -onpulse "$(p["bin_st"]) $(p["bin_end"])" -onpulsed "/NULL" -p3foldd "/NULL" -w -oformat ascii $debased_file`,  stderr="errs.txt"))
