b048444a20f4767fe078b9ca84d100ae9190020f
Andrzej Szary <aszary@gmail.com>
2025-10-02 12:41:10 +0200

    zmiany

--- modules/data.jl
+++ modules/data.jl
@@ -292,10 +292,8 @@ module Data
         # debase the data
         debase(outfile, params_file, p)
 
-        println("1---")
         # Calculate 2dfs and lrfs
         twodfs_lrfs(debased_file, params_file, p; detect=false)
-        println("2---")
 
         # calculate p3-folded profile
         run(pipeline(`pfold  -p3fold "$(p["p3"]) $(p["p3_ybins"])" -onpulse "$(p["bin_st"]) $(p["bin_end"])" -onpulsed "/NULL" -p3foldd "/NULL" -w -oformat ascii $debased_file`,  stderr="errs.txt"))
@@ -322,10 +320,12 @@ module Data
         # LRFS
         d3 = load_ascii_all(joinpath(data_dir, "pulsar.debase.lrfs"))
         Plot.lrfs(d3, data_dir, p; name_mod="pulsar", show_=false)
-
+        println("1----")
         # P3-folded
         d5 = Data.load_ascii(joinpath(data_dir, "pulsar.debase.p3fold"))
+        println("2----")
         Plot.p3fold(d5, data_dir; start=3, bin_st=p["bin_st"]-20, bin_end=p["bin_end"]+20, name_mod="pulsar", show_=false, repeat_num=4)
+        println("3----")
  
     end
 
