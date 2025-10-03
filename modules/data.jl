5f17c31ae37c0f15c3e360aab93e9b0132d69024
Andrzej Szary <aszary@gmail.com>
2025-10-03 14:36:16 +0200

    zmiany

--- modules/data.jl
+++ modules/data.jl
@@ -314,7 +314,7 @@ module Data
         Plot.single(da2, outdir; darkness=0.7, number=nothing, bin_st=p["bin_st"], bin_end=p["bin_end"], start=1, name_mod="cleaned", show_=true)
 
         folded = Tools.p3fold(da2, p["p3"],  p["p3_ybins"])
-        Plot.single(folded, outdir; darkness=0.95, number=nothing, bin_st=p["bin_st"], bin_end=p["bin_end"], start=1, name_mod="p3fold_clean", show_=true, repeat_num=4)
+        Plot.single(folded, outdir; darkness=0.97, number=nothing, bin_st=p["bin_st"], bin_end=p["bin_end"], start=1, name_mod="p3fold_clean", show_=true, repeat_num=4)
 
         #= 
         =#
