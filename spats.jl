5660f0a69e2b1c7ba77f1a0d52084fea2d51ff12
Czarek Kruszewski <czkk18@gmail.com>
2025-10-08 22:09:55 +0200

    inny pulsar

--- spats.jl
+++ spats.jl
@@ -96,7 +96,7 @@ module SpaTs
         #J1539_6322_Sard("/home/psr/data/new/J1539-6322/2020-04-11-23:52:16/", vpmout)
 
         #process_psrdata(vpmout*"J1921+2153", vpmout*"J1921+2153")
-        process_psrdata(vpmout*"J1901-0906", vpmout*"J1901-0906")
+        process_psrdata(vpmout*"J1741-0840", vpmout*"J1741-0840")
 
         #Data.process_all_data(vpmout)
         #Data.combine_pngs_to_pdf(vpmout)
