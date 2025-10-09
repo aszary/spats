c09cccd80198326ceda59c969f934c57c46bb94a
Andrzej Szary <aszary@gmail.com>
2025-10-09 12:13:25 +0200

    zmiany

--- spats.jl
+++ spats.jl
@@ -100,12 +100,12 @@ module SpaTs
         #process_psrdata(vpmout*"J2139+2242", vpmout*"J2139+2242") # moderate
         #process_psrdata(vpmout*"J0820-1350", vpmout*"J0820-1350") # nice
 
-        #process_psrdata(vpmout*"J1834-0010", vpmout*"J1834-0010") # P. nice
+        process_psrdata(vpmout*"J1834-0010", vpmout*"J1834-0010") # P. nice
         #process_psrdata(vpmout*"J0034-0721", vpmout*"J0034-0721") # P. nice
         #process_psrdata(vpmout*"J0823+0159", vpmout*"J0823+0159") #  nice single pulses, but p3folds no
         #process_psrdata(vpmout*"J1842-0359", vpmout*"J1842-0359") #  nice single pulses, nice P. p3fold
         #process_psrdata(vpmout*"J1034-3224", vpmout*"J1034-3224") # single bad, p3folds bad, P3 stable
-        process_psrdata(vpmout*"J1133-6250", vpmout*"J1133-6250") #
+        #process_psrdata(vpmout*"J1133-6250", vpmout*"J1133-6250") # single not stable, P. p3fold (10 ybins)
 
 
 
