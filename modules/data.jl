af8226c7ca978edd32574d86ee1b4ccbbbba6840
Andrzej Szary <aszary@gmail.com>
2025-10-16 12:03:59 +0200

    zmiany

--- modules/data.jl
+++ modules/data.jl
@@ -237,6 +237,8 @@ module Data
 
         # Calculate 2dfs and lrfs
         run(pipeline(`pspec -w -oformat ASCII -2dfs -lrfs -profd "/NULL" -onpulsed "/NULL" -2dfsd "/NULL" -lrfsd "/NULL" -nfft $(p["nfft"]) -onpulse "$(p["bin_st"]) $(p["bin_end"])" $debased_file`,  stderr="errs.txt"))
+        
+        println("\n\n TUTAJ2\n\n", debased_file)
 
         if p["p3"] == -1.0 || detect == true
             # Find P3
