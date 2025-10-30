2e0d3ddb5c38a883ec6035506bf02a0cbbf41e55
Andrzej Szary <aszary@gmail.com>
2025-10-30 09:55:08 +0100

    zmiany

--- modules/data.jl
+++ modules/data.jl
@@ -754,7 +754,8 @@ difference = da1_norm .- da2_norm
         # TODO mv pulsar.debase.gg to low...
 
         # Calculate 2dfs and lrfs
-        twodfs_lrfs(low_filename, params_file, p; detect=true)
+        println(low_filename)
+        #twodfs_lrfs(low_filename, params_file, p; detect=true)
         #twodfs_lrfs(high_filename, params_file, p; detect=true)
 
 
