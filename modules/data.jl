9071dbdd802498cd5b686ce072536c3a612160b0
Andrzej Szary <aszary@gmail.com>
2025-11-05 11:29:49 +0100

    zmiany

--- modules/data.jl
+++ modules/data.jl
@@ -715,12 +715,18 @@ module Data
         folded_high = Tools.p3fold(da_high, p["p3"],  p["p3_ybins"])
         Plot.single(folded_high, outdir; darkness=0.9, number=nothing, bin_st=p["bin_st"], bin_end=p["bin_end"], start=1, name_mod="p3fold_high", show_=false, repeat_num=4)
 
-        # PSRSALSA p3folding
+        # PSRSALSA p3folding low freq
         println("pfold -p3fold_norefine -p3fold \"$(p["p3"]) $(p["p3_ybins"])\" -onpulse \"$(p["bin_st"]) $(p["bin_end"])\" -onpulsed \"/NULL\" -p3foldd \"/NULL\" -w -oformat ascii $low_debase")
         run(pipeline(`pfold -p3fold_norefine -p3fold "$(p["p3"]) $(p["p3_ybins"])" -onpulse "$(p["bin_st"]) $(p["bin_end"])" -onpulsed "/NULL" -p3foldd "/NULL" -w -oformat ascii $low_debase`,  stderr="errs.txt"))
         d5 = Data.load_ascii(replace(low_debase, ".gg"=>".p3fold"))
-        Plot.p3fold(d5, outdir; start=1, bin_st=p["bin_st"]-20, bin_end=p["bin_end"]+20, darkness=0.9, name_mod="pulsar", show_=true, repeat_num=4)
+        Plot.p3fold(d5, outdir; start=1, bin_st=p["bin_st"]-20, bin_end=p["bin_end"]+20, darkness=0.9, name_mod="pulsar_low", show_=true, repeat_num=4)
  
+        # PSRSALSA p3folding high freq
+        run(pipeline(`pfold -p3fold_norefine -p3fold "$(p["p3"]) $(p["p3_ybins"])" -onpulse "$(p["bin_st"]) $(p["bin_end"])" -onpulsed "/NULL" -p3foldd "/NULL" -w -oformat ascii $high_debase`,  stderr="errs.txt"))
+        d5 = Data.load_ascii(replace(high_debase, ".gg"=>".p3fold"))
+        Plot.p3fold(d5, outdir; start=1, bin_st=p["bin_st"]-20, bin_end=p["bin_end"]+20, darkness=0.9, name_mod="pulsar_high", show_=true, repeat_num=4)
+ 
+
 
 
         return p, nothing, outdir
