2bbf4fc7a7c2ceb8fb588ab45a7b39252c1c3e99
Maciej <macszn@st.amu.edu.pl>
2025-11-13 13:34:51 +0100

    J0725-1635

--- spats.jl
+++ spats.jl
@@ -126,8 +126,8 @@ module SpaTs
         #process_psrdata_16("/home/psr/data/new/J1720-0212/2019-09-29-12:53:17", vpmout*"J1720-0212_16") # start diging there...
         #process_psrdata_16("/home/psr/data/new/J0711+0931/2020-07-04-09:47:10", vpmout*"J0711+0931_16") # start diging there... # start diging there... 
         #process_psrdata_16("/home/psr/data/new/J1944+1755/2020-07-13-00:03:52", vpmout*"J1944+1755_16")
-        process_psrdata_16("/home/psr/data/new/J1727-2739/2019-11-26-16:01:01", vpmout*"J1727-2739_16") #was redone
-        #process_psrdata_16("/home/psr/data/new/J0725-1635/2019-10-27-23:06:09", vpmout*"J0725-1635_16")
+        #process_psrdata_16("/home/psr/data/new/J1727-2739/2019-11-26-16:01:01", vpmout*"J1727-2739_16") #was redone
+        process_psrdata_16("/home/psr/data/new/J0725-1635/2019-10-27-23:06:09", vpmout*"J0725-1635_16")
         #process_psrdata_16("/home/psr/data/new/J1807+0756/2020-06-07-22:03:05", vpmout*"J1807+0756_16") #zapping needed 
         #process_psrdata_16("/home/psr/data/new/J1220-6318/2019-07-25-17:28:57", vpmout*"J1220-6318_16")
         #process_psrdata_16("/home/psr/data/new/J1750-3503/2019-12-14-14:22:12", vpmout*"J1750-3503_16")
