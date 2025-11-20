87a7c3840ceaa763f7b20c68495fefa8b91d1f7d
Czarek Kruszewski <czkk18@gmail.com>
2025-11-20 13:07:54 +0100

    zmiany

--- spats.jl
+++ spats.jl
@@ -133,8 +133,8 @@ module SpaTs
         #process_psrdata_16("/home/psr/data/new/J1133-6250/2019-11-06-00:46:43/", vpmout*"J1133-6250_16")
         #process_psrdata_16("/home/psr/data/new/J1821+1715/2020-06-08-02:20:21/", vpmout*"J1821+1715_16")
         #process_psrdata_16("/home/psr/data/new/J1312-5402/2020-03-30-19:11:06/", vpmout*"J1312-5402_16")
-        #process_psrdata_16("/home/psr/data/new/J1919+0134/2020-02-02-11:45:29/", vpmout*"J1919+0134_16")
-        Data.analyse_p3folds_16(vpmout*"J1919+0134_16", "refine")
+        process_psrdata_16("/home/psr/data/new/J1232-4742/2020-04-11-22:21:18/", vpmout*"J1232-4742_16")
+        #Data.analyse_p3folds_16(vpmout*"J1919+0134_16", "refine")
 
         #Data.process_all_data(vpmout)
         #Data.combine_pngs_to_pdf(vpmout)
