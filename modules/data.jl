8bd5facf0a457aa079a237426267f81f7cae5a54
Andrzej Szary <aszary@gmail.com>
2025-11-25 13:38:01 +0100

    zmiany

--- modules/data.jl
+++ modules/data.jl
@@ -907,6 +907,7 @@ module Data
         end
 
         # speeding up
+        #=
         # add all .spCf16 files 
         add_psrfiles(indir, outfile; files=files, sixteen=true)
 
@@ -921,6 +922,7 @@ module Data
         # debase the data
         debase_16(low_filename, high_filename, params_file, p)
 
+        =#
         # speeding ends here
 
         low_filename = "/home/psr/output/J2139+2242_single/pulsar.low"
@@ -929,7 +931,7 @@ module Data
         #Tools.generate_snr(low_debase) 
         low_snrfile = low_debase * ".snr.txt"
 
-        twodfs_lrfs(low_debase, params_file, p; detect=false) # just to detect p2
+        # twodfs_lrfs(low_debase, params_file, p; detect=false) # just to detect p2
 
         # single pulses low
         da_lo = Data.load_ascii(replace(low_filename, ".low"=>"_low_debase.txt"))
