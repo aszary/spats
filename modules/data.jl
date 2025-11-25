bad817e3d6b509fa35b574b8760f9750c72663e3
Andrzej Szary <aszary@gmail.com>
2025-11-25 11:26:00 +0100

    zmiany

--- modules/data.jl
+++ modules/data.jl
@@ -927,7 +927,7 @@ module Data
         # single pulses low
         da_lo = Data.load_ascii(replace(low_filename, ".low"=>"_low_debase.txt"))
         
-        Plot.test_track_subpulses_snr_new(da_lo, outdir, 10, low_snrfile, bin_st=p["bin_st"], bin_end=p["bin_end"])
+        #Plot.test_track_subpulses_snr_new(da_lo, outdir, 10, low_snrfile, bin_st=p["bin_st"], bin_end=p["bin_end"])
 
         high_debase = replace(high_filename, ".high"=>"_high.debase.gg")
         Tools.generate_snr(high_debase) 
