43855e292c4faafbe553f13909c2f462084e2d4e
Andrzej Szary <aszary@gmail.com>
2025-11-27 11:56:40 +0100

    zmiany

--- modules/tools.jl
+++ modules/tools.jl
@@ -2163,7 +2163,7 @@ module Tools
         if p2_bins % 2 == 0
             p2_bins += 1  # we force an odd length
         end
-        σ = p2_bins / 2 / 2.35482 /10
+        σ = p2_bins / 2 / 2.35482 /5
         kernel = gauss(collect(1:p2_bins), [1, p2_bins/2, σ, 0])
 
 
