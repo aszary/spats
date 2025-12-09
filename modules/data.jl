bfa348cb984d7420514ed0e7b30ddd34a99de4fb
Andrzej Szary <aszary@gmail.com>
2025-12-09 10:16:35 +0100

    zmiany

--- modules/data.jl
+++ modules/data.jl
@@ -907,7 +907,7 @@ module Data
         end
 
         # speeding up
-
+        #=
         # add all .spCf16 files 
         add_psrfiles(indir, outfile; files=files, sixteen=true)
 
@@ -923,10 +923,10 @@ module Data
         debase_16(low_filename, high_filename, params_file, p)
 
         # speeding ends here
-
+        =#
         # TODO remove those two
-        #low_filename = "/home/psr/output/J2139+2242_single/pulsar.low"
-        #high_filename = "/home/psr/output/J2139+2242_single/pulsar.high"
+        low_filename = "/home/psr/output/J2139+2242_single/pulsar.low"
+        high_filename = "/home/psr/output/J2139+2242_single/pulsar.high"
 
         low_debase = replace(low_filename, ".low"=>"_low.debase.gg")
         Tools.generate_snr(low_debase) 
