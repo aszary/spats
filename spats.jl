6d96600e7f29610abb2f393f3332e2b56ee891f4
Maciej <macszn@st.amu.edu.pl>
2026-01-08 13:11:40 +0100

    uhf+uhf16

--- spats.jl
+++ spats.jl
@@ -98,32 +98,22 @@ module SpaTs
         #test2(vpmout)
         
         
-        process_psrdata("/home/psr/data/uhf/J0823+0159/2023-02-27-20:38:58/816/single/", vpmout*"J0823+0159_UHF") # uhf
-        #process_psrdata(vpmout*"J0630-2834", vpmout*"J0630-2834") # not nice
-        #process_psrdata(vpmout*"J2139+2242", vpmout*"J2139+2242") # moderate
-        #process_psrdata(vpmout*"J0820-1350", vpmout*"J0820-1350") # nice
+        process_psrdata("/home/psr/data/uhf/J0823+0159/2023-02-27-20:38:58/816/single/", vpmout*"J0823+0159_uhf")# uhf
+        process_psrdata_16("/home/psr/data/uhf/J0823+0159/2023-02-27-20:38:58/816/single/", vpmout*"J0823+0159_16_uhf") 
+
 
         #process_psrdata(vpmout*"J1834-0010", vpmout*"J1834-0010") # P. nice
 
 
 
         #process_psrdata_16("/home/psr/data/new/J1414-6802/2020-07-24-16:17:58/", vpmout*"J1414-6802_16") # start diging there...
-        #process_psrdata_16("/home/psr/data/new/J1539-6322/2020-04-11-23:52:16/", vpmout*"J1539-6322_16") 
-        #process_psrdata_16("/home/psr/data/new/J1514-4834/2019-12-16-08:22:16/", vpmout*"J1514-4834_16")
-        #process_psrdata_16("/home/psr/data/new/J0034-0721/2019-10-18-22:29:51/", vpmout*"J0034-0721_16")
-        #process_psrdata_16("/home/psr/data/new/J0823+0159/2019-11-05-23:33:48/", vpmout*"J0823+0159_16")
-        #process_psrdata_16("/home/psr/data/new/J1842-0359/2019-11-05-18:03:43/", vpmout*"J1842-0359_16")
-        #process_psrdata_16("/home/psr/data/new/J1034-3224/2019-10-19-07:31:26/", vpmout*"J1034-3224_16")
-        #process_psrdata_16("/home/psr/data/new/J1133-6250/2019-11-06-00:46:43/", vpmout*"J1133-6250_16")
-        #process_psrdata_16("/home/psr/data/new/J1555-3134/2019-10-31-06:34:39/", vpmout*"J1555-3134_16")
-        #Data.analyse_p3folds_16(vpmout*"J1555-3134_16", "refine")
-        #process_psrdata_16("/home/psr/data/new/J1312-5402/2020-03-30-19:11:06/", vpmout*"J1312-5402_16")
 
         # checking single pulses offset
-
-
         #process_psrdata_single("/home/psr/data/new/J0823+0159/2019-11-05-23:33:48/", vpmout*"J0823+0159_single") # no visible offset
         
+
+
+
 
 
 
