6cffb241c4237a4477b0ee315ef4ad7d9a9419cd
Andrzej Szary <aszary@gmail.com>
2026-01-13 14:22:58 +0100

    zmiany

--- spats.jl
+++ spats.jl
@@ -109,7 +109,8 @@ module SpaTs
         # TODO work on that one
         #process_psrdata_16("/home/psr/data/uhf/J1842-0359/2020-07-25-19:58:19/816/single/", vpmout*"J1842-0359_uhf_16") 
         #Data.analyse_p3folds_16(vpmout*"J1842-0359_16", "refine")
-        Data.analyse_p3folds_16(vpmout*"J1842-0359_uhf_16", "norefine")
+        #Data.analyse_p3folds_16(vpmout*"J1842-0359_uhf_16", "norefine")
+        Data.analyse_p3folds_16(vpmout*"J1842-0359_uhf_16", "refine")
 
 
         #process_psrdata(vpmout*"J2139+2242", vpmout*"J2139+2242") # moderate
