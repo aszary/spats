cdd3db1c17948e339c05d6acfb69613ad4e1c30d
Maciej <macszn@st.amu.edu.pl>
2026-02-26 00:51:09 +0100

    J1921+1948

--- spats.jl
+++ spats.jl
@@ -110,9 +110,9 @@ module SpaTs
 
     
 
-        process_psrdata_16("/home/psr/data/new/J1900-0933/2020-03-23-03:01:05", vpmout*"J1900-0933_16")
+        process_psrdata_16("/home/psr/data/new/J1921+1948/2019-12-16-14:44:43", vpmout*"J1921+1948_16")
         
-        Data.analyse_p3folds_16(vpmout*"J1900-0933_16", "norefine")
+        Data.analyse_p3folds_16(vpmout*"J1921+1948_16", "norefine")
 
         #process_psrdata_16("/home/psr/data/new/J1414-6802/2020-07-24-16:17:58/", vpmout*"J1414-6802_16") # start diging there...
 
