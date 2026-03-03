8172a395a331ba6f930dd87cf29633cbd132678d
Andrzej Szary <aszary@gmail.com>
2026-03-03 10:57:39 +0100

    zmiany

--- spats.jl
+++ spats.jl
@@ -220,7 +220,11 @@ module SpaTs
 
         # work on pulsar J1534-4428
         #process_psrdata_16("/home/psr/data/new/J1534-4428/2020-01-03-01:03:02/", vpmout*"J1534-4428_16")
-        Data.analyse_p3folds_16(vpmout*"J1534-4428_16", "norefine")
+        #Data.analyse_p3folds_16(vpmout*"J1534-4428_16", "norefine")
+
+        # work on pulsar J1857+0057
+        process_psrdata_16("/home/psr/data/new/J1857+0057/2020-02-03-06:00:48/", vpmout*"J1857+0057_16")
+        #Data.analyse_p3folds_16(vpmout*"J1857+0057_16", "norefine")
 
 
 
