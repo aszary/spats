b43c2b7fb44de68d448a9a5ca1c389155097572e
Andrzej Szary <aszary@gmail.com>
2026-03-03 13:49:43 +0100

    zmiany

--- modules/data.jl
+++ modules/data.jl
@@ -866,6 +866,15 @@ module Data
         println("Mean shift: $(mean(shifts)) bins")
         println("Median shift: $(median(shifts)) bins")
 
+        profile_idx = 1
+        p1 = nl[profile_idx, :]
+        p2 = nh[profile_idx, :]
+        bins = 1:length(profile)
+
+        writedlm(joinpath(indir,"p_low.txt"), hcat(bins, p1), ' ')
+        writedlm(joinpath(indir,"p_high.txt"), hcat(bins, p1), ' ')
+
+
         println(size(nl))
 
         #Plot.analyse_p3folds(nl, nh, p)
