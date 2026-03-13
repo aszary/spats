4b81caec2e676885d8fcc01c4a689252792c6224
Andrzej Szary <aszary@gmail.com>
2026-03-13 15:39:13 +0100

    zmiany

--- spats.jl
+++ spats.jl
@@ -201,7 +201,7 @@ module SpaTs
         # work on pulsar J2053-7200
         #process_psrdata_16("/home/psr/data/new/J2053-7200/2019-11-26-18:06:44/", vpmout*"J2053-7200_16")
         #Data.analyse_p3folds_16(vpmout*"J2053-7200_16", "norefine")
-        #Data.analyse_p3folds_16_new(vpmout*"J2053-7200_16", "norefine")
+        Data.analyse_p3folds_16_new(vpmout*"J2053-7200_16", "norefine")
 
 
         # work on pulsar J1232-4742
