21b0f55bb0a5724ddc2bf934a6f2fe4a3640f372
Maciej <macszn@st.amu.edu.pl>
2026-03-26 19:16:01 +0100

    J2139+2242

--- spats.jl
+++ spats.jl
@@ -111,8 +111,8 @@ module SpaTs
         #process_psrdata_16("/home/psr/data/new/J1842-0359/2019-11-05-18:03:43/", vpmout*"J1842-0359_16")
         #Data.analyse_p3folds_16(vpmout*"J1842-0359_16", "refine")
         #Data.analyse_p3folds_16(vpmout*"J1842-0359_16", "norefine")
-        Data.analyse_p3folds_16_new(vpmout*"J1842-0359_16", "norefine")
-        Data.analyse_offsets(vpmout*"J1842-0359_16", "norefine", 1.841; n_comp=4)
+        Data.analyse_p3folds_16_new(vpmout*"J2139+2242_16", "norefine")
+        Data.analyse_offsets(vpmout*"J2139+2242_16", "norefine", 0.2569; n_comp=2)
             # uhf
         #process_psrdata("/home/psr/data/uhf/J1842-0359/2020-07-25-19:58:19/816/single/", vpmout*"J1842-0359_uhf")
             # uhf multifrequency
