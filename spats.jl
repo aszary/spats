9a984751762e464ffc98cfeecb42c2a26b8f84bc
Andrzej Szary <aszary@gmail.com>
2026-04-20 12:45:44 +0200

    zmiany

--- spats.jl
+++ spats.jl
@@ -154,9 +154,9 @@ module SpaTs
         #process_psrdata(vpmout*"J1034-3224", vpmout*"J1034-3224") # single bad, p3folds bad, P3 stable
 
         # PSR J1110-5637  # TODO HERE
-        #process_psrdata_16("/home/psr/data/new/J1110-5637/2019-10-19-08:18:28/", vpmout*"J1110-5637_16")
+        process_psrdata_16("/home/psr/data/new/J1110-5637/2019-10-19-08:18:28/", vpmout*"J1110-5637_16")
         #Data.analyse_p3folds_16(vpmout*"J1110-5637_16", "norefine")
-        Data.analyse_p3folds_16_new(vpmout*"J1110-5637_16", "norefine"; n_comp=2)
+        #Data.analyse_p3folds_16_new(vpmout*"J1110-5637_16", "norefine"; n_comp=2)
 
 
 
