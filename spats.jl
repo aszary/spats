04e917d2d0650a13097165047b05617c6709c6e4
Andrzej Szary <aszary@gmail.com>
2026-04-20 12:50:19 +0200

    zmiany

--- spats.jl
+++ spats.jl
@@ -156,7 +156,10 @@ module SpaTs
         # PSR J1110-5637  # TODO HERE
         #process_psrdata_16("/home/psr/data/new/J1110-5637/2019-10-19-08:18:28/", vpmout*"J1110-5637_16")
         #Data.analyse_p3folds_16(vpmout*"J1110-5637_16", "norefine")
-        Data.analyse_p3folds_16_new(vpmout*"J1110-5637_16", "norefine"; n_comp=2)
+        #Data.analyse_p3folds_16_new(vpmout*"J1110-5637_16", "norefine"; n_comp=2)
+        Data.position_angle(vpmout*"J1110-5637_16")
+        #Data.geometry_analysis(vpmout*"J1110-5637_16")
+
 
 
 
