e911b400fbd372ada8ee20107b0afcba9021a267
Andrzej Szary <aszary@gmail.com>
2026-04-23 09:51:22 +0200

    zmiany

--- spats.jl
+++ spats.jl
@@ -167,7 +167,7 @@ module SpaTs
         #Data.analyse_p3folds_16(vpmout*"J1048-5832_16", "norefine")
         #Data.analyse_p3folds_16_new(vpmout*"J1048-5832_16", "norefine"; n_comp=2)
         #Data.position_angle(vpmout*"J1048-5832_16")
-        Data.geometry_analysis(vpmout*"J1048-5832_16")
+        #Data.geometry_analysis(vpmout*"J1048-5832_16")
 
 
 
