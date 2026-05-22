5470b7bdebaf8a24c81eb551b9d4772c2aa7d916
Andrzej Szary <aszary@gmail.com>
2026-05-22 08:37:03 +0200

    zmiany

--- spats.jl
+++ spats.jl
@@ -139,8 +139,8 @@ module SpaTs
         #Data.geometry_analysis(vpmout*"J0034-0721_16")
 
         # PSR J0134-2937
-        process_psrdata_16("/home/psr/data/new/J0134-2937/2019-10-18-22:46:59/", vpmout*"J0134-2937_16")
-        Data.analyse_p3folds_16_new(vpmout*"J0134-2937_16", "norefine"; n_comp=2)
+        #process_psrdata_16("/home/psr/data/new/J0134-2937/2019-10-18-22:46:59/", vpmout*"J0134-2937_16")
+        #Data.analyse_p3folds_16_new(vpmout*"J0134-2937_16", "norefine"; n_comp=2)
 
 
         # PSR J0151-0635
@@ -336,7 +336,7 @@ module SpaTs
 
         # PSR J1903+2225
         #process_psrdata_16("/home/psr/data/new/J1903+2225/2020-08-20-20:53:16/", vpmout*"J1903+2225_16")
-        Data.analyse_p3folds_16_new(vpmout*"J1903+2225_16", "norefine"; n_comp=1)
+        #Data.analyse_p3folds_16_new(vpmout*"J1903+2225_16", "norefine"; n_comp=1)
 
         # PSR J1904-1224
         #process_psrdata_16("/home/psr/data/new/J1904-1224/2020-04-14-05:57:45", vpmout*"J1904-1224_16")
