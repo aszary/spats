da3622792640b290e014495e0137d7a65030551d
Andrzej Szary <aszary@gmail.com>
2026-06-19 14:35:46 +0200

    zmiany

--- spats.jl
+++ spats.jl
@@ -360,8 +360,8 @@ module SpaTs
         #process_psrdata_16("/home/psr/data/new/J1133-6250/2019-11-06-00:46:43/", vpmout*"J1133-6250_16")
         #process_psrdata_single("/home/psr/data/new/J1133-6250/2019-11-06-00:46:43/", vpmout*"J1133-6250_single") # some offset? probably not
         #process_psrdata(vpmout*"J1133-6250", vpmout*"J1133-6250") # single not stable, P. p3fold (10 ybins)
-        #phase_modulation(vpmout*"J1133-6250")
-        p3fold_coherent(vpmout*"J1133-6250")
+        phase_modulation(vpmout*"J1133-6250")
+        #p3fold_coherent(vpmout*"J1133-6250")
 
         # PSR J1137-6700
         #process_psrdata_16("/home/psr/data/new/J1137-6700/2019-11-06-00:36:02/", vpmout*"J1137-6700_16")
