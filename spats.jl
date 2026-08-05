9dbf7c7cdaea769824ffd950a51b5345979dabad
Andrzej Szary <aszary@gmail.com>
2026-08-05 11:13:09 +0200

    zmiany

--- spats.jl
+++ spats.jl
@@ -590,12 +590,12 @@ module SpaTs
         # PSR J1429-5935
         #process_psrdata("/home/psr/data/new/J1429-5935/2020-02-14-04:21:11", vpmout*"J1429-5935")
         #phase_modulation(vpmout*"J1429-5935")
-        p3fold_coherent(vpmout*"J1429-5935")
+        #p3fold_coherent(vpmout*"J1429-5935")
 
         # PSR J1750-3503
         #process_psrdata("/home/psr/data/new/J1750-3503/2019-12-14-14:22:12/", vpmout*"J1750-3503")
         #phase_modulation(vpmout*"J1750-3503")
-        #p3fold_coherent(vpmout*"J1750-3503")
+        p3fold_coherent(vpmout*"J1750-3503")
 
 
         
