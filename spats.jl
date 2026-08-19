dc3ca96cc4f272c9f4e5e2b89e7dc8794e59e173
Maciej <macszn@st.amu.edu.pl>
2026-08-19 11:35:16 +0200

    J1844-0433

--- spats.jl
+++ spats.jl
@@ -630,12 +630,14 @@ end
 
         #Tools.clean_all(vpmout)
         # Start from J1609-4616, then downward toward J1500, J1400, … 
-        analyse_all_2()
+        #analyse_all_2()
 
         #how to do with zapping below
         #process_psrdata_16("/home/psr/data/new/J1915+0752/2020-05-06-03:23:45", vpmout*"J1915+0752_16")
         #Data.analyse_p3folds_16_new(vpmout*"J1915+0752_16", "norefine"; n_comp=1)
         
+        process_psrdata_16("/home/psr/data/new/J1844-0433/2020-01-21-08\:30\:46", vpmout*"J1844-0433_16")
+        Data.analyse_p3folds_16_new(vpmout*"J1844-0433_16", "norefine"; n_comp=1)
     end
 
 end # module
