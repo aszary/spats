99f4e1f8eeed3c339b4ab9f9a4a2f67fe55e5062
Maciej <macszn@st.amu.edu.pl>
2026-09-23 13:17:21 +0200

    redoo

--- spats.jl
+++ spats.jl
@@ -850,7 +850,7 @@ Phase-drift vs amplitude-modulation test on already-processed data.
         
         
         
-        #phase_modulation3(vpmout*"J1750-3503"; show_=true)
+        phase_modulation3(vpmout*"J1750-3503_16"; show_=true)
         phase_modulation3_list("output", "input/pulsars.txt")
 
 
