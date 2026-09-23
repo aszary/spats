4706e468653ef561c9251035632edc623a888e11
Maciej <macszn@st.amu.edu.pl>
2026-09-23 14:38:42 +0200

    lowercase

--- spats.jl
+++ spats.jl
@@ -852,7 +852,7 @@ Phase-drift vs amplitude-modulation test on already-processed data.
         
         phase_modulation3(vpmout*"J1750-3503"; show_=true)
         #phase_modulation3_list(vpmout, "input/pulsars.txt")
-        Travel.travel_test(vpmout*"J0820-1350"; max_lag=15, show_=false)
+        travel.travel_test(vpmout*"J0820-1350"; max_lag=15, show_=false)
 
 
     end
